f7ad30f3b008f18029063b7798ba4abbb54a68e7
ManihaHashmi2511 <it.solution8800@gmail.com>
2026-06-08 10:27:46 +0500

    vercel fix

--- server.js
+++ server.js
@@ -19,5 +19,9 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
-const PORT = process.env.PORT || 5000;  
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));  
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT} 🚀`));
+}
+
+module.exports = app;
